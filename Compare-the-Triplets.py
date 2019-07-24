8ba44082730e27a6e7eba827fb38af8b3d5cd4c0
Agent Kaine <agent_kaine@tutanota.com>
2019-07-24 20:57:04 +0530

    Added Compare-the-Triplets.py

---
+++ Compare-the-Triplets.py
@@ -0,0 +1,28 @@
+import math
+import os
+import random
+import re
+import sys
+
+def compareTriplets(a, b):
+    ar = [0, 0]
+    for i in range(3):
+        if a[i] > b[i]:
+            ar[0] += 1
+        if b[i] > a[i]:
+            ar[1] += 1
+    return ar
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
